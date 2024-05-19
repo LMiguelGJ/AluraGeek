1e6e08ac9989d3389198f2d7e651e557f802b7b4
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-05-18 20:14:54 -0400

    changes in the order list

--- js/scrips.js
+++ js/scrips.js
@@ -58,6 +58,7 @@ async function listaProductos() {
   try {
     const listaAPI = await conectaAPI.listaProductos();
     lista.innerHTML = ``;
+    listaAPI.reverse();
     listaAPI.forEach(element => lista.appendChild(construyeCard(element.id, element.nombre, element.precio, element.imagen)));
   } catch {
     lista.innerHTML = `<h2 class="mensaje__titulo">Ha ocurrido un problema con la conexion :(</h2>`;
