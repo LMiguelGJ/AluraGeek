7b0802f4add54184c5bb11f30c271e351e4f008d
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-05-20 13:46:06 -0400

    add sever

--- api/server.js
+++ api/server.js
@@ -8,13 +8,8 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
 server.use(router)
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log('JSON Server is running')
 })
 
