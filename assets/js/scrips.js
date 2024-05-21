f2a1cc5b98a14ca414440674f49a39e6798f785d
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-05-21 12:59:29 -0400

    changes for fix error

--- assets/js/scrips.js
+++ assets/js/scrips.js
@@ -55,14 +55,13 @@ function construyeCard(id,nombre,precio,imagen){
 
 async function listaProductos() {
   try {
-    const listaAPI = await conectaAPI.listaProductos();
+    let listaAPI = await conectaAPI.listaProductos();
     console.log(listaAPI)
     lista.innerHTML = ``;
     listaAPI.reverse();
     listaAPI.forEach(element => lista.appendChild(construyeCard(element.id, element.nombre, element.precio, element.imagen)));
-    listaAPI = {}
-  } catch {
-    lista.innerHTML = `<h2 class="mensaje__titulo">Ha ocurrido un problema con la conexion :(</h2>`;
+  } catch (error) {
+    lista.innerHTML = `<h2 class="mensaje__titulo">Ha ocurrido un problema con la conexion ${error} :(</h2>`;
   }
 }
 
